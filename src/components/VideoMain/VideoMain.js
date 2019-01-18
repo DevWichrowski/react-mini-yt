db187b68b556d810fb3b313e53efeb15c274ed90
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-18 20:50:04 +0100

    Add player and react bootstrap

--- src/components/VideoMain/VideoMain.js
+++ src/components/VideoMain/VideoMain.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './VideoMain.scss';
 import {getVideos} from "../../store/actions/searchVideoAction";
 import connect from "react-redux/es/connect/connect";
+import YouTube from 'react-youtube';
 
 class VideoMain extends Component {
     componentDidMount() {
@@ -9,14 +10,19 @@ class VideoMain extends Component {
     }
 
     render() {
-        const getData = () => {
-            console.log(this.props.videosData.items[0].snippet.title);
-        }
+        const playerSettings = {
+            playerVars: {
+                autoplay: 1
+            }
+        };
 
         return (
-            <div className={"VideoMain"}>
+            <div className="VideoMain">
                 VideoMain
-                <button onClick={() => getData()}>Get data</button>
+                <div className="video-details">
+                    <YouTube className="main-video" videoId="eHBws27NXqI" opts={playerSettings}/>
+                    <h2></h2>
+                </div>
             </div>
         );
     }
