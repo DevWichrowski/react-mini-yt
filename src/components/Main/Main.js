97c355fe375f1bbb029bbf3378c088ed16363780
Radek <laser242424@wp.pl>
2019-01-18 18:31:23 +0100

    header fix

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Main.scss';
 import connect from "react-redux/es/connect/connect";
 import {getVideos} from "../../store/actions/searchVideoAction";
+import Header from '../Header/Header'
 
 // Tutaj beda znajdowały sie wszystko komponenty z nawigacja, App bedzie tylko wrapował Main
 function Main(props) {
     return (
         <div className="Main">
+            <Header />
             <h1>Main component</h1>
                 <button onClick={() => props.getVideos('Tibia')}>Click to test API</button>
         </div>
