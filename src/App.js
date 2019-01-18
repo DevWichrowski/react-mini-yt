0e1b6e9d444574584934cb7f9eb466cc0b61fc9d
Radek <laser242424@wp.pl>
2019-01-18 17:27:01 +0100

    Material-UI included.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Main from "./components/Main/Main";
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
+        <Button variant="contained" color="primary" >Material-ui Test</Button>
       </div>
     );
   }
