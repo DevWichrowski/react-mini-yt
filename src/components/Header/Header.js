8f30db591986873a6ea3994de767b49ccd7b3ab5
mavrise <dorotazielwow@gmail.com>
2019-01-19 13:32:15 +0100

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,7 @@ import logo from '../../assets/logo.png'
 import { connect } from 'react-redux'
 import { getVideos, saveMoveTitle } from '../../store/actions/searchVideoAction'
 
-
-<<<<<<< HEAD
 class Header extends Component {
-=======
-class Header extends React.Component {ł
->>>>>>> dorota
     constructor(props) {
         super(props)
 
@@ -29,40 +24,6 @@ class Header extends React.Component {ł
 
         return (
             <div className="Container">
-<<<<<<< HEAD
-                <Navbar fixedTop={true} >
-                    <Grid>
-                        <Row>
-                            <Col xs={2} sm={2} md={2} lg={2} >
-                                <img src={ytlogo} alt="YouTube" className="img" />
-                            </Col>
-                            <Col xs={8} sm={8} md={8} lg={8} >
-                                <input
-                                    onChange={this.saveTitleInState}
-                                    className="input"
-                                    placeholder="Szukaj..."
-                                    onKeyPress={event => {
-                                        if (event.key === 'Enter') {
-                                            this.props.saveTitleToStore(this.state.value)
-                                        }
-                                        if (event.key === 'Enter') {
-                                            this.props.searchMovie(this.state.value)
-                                        }
-                                    }
-                                    }
-                                />
-                                <Button className="Button"
-                                    onClick={() => {
-                                        this.props.saveTitleToStore(this.state.value)
-                                        this.props.searchMovie(this.state.value)
-                                    }}> <FontAwesomeIcon icon={faSearch} /> </Button>
-                            </Col>
-                        </Row>
-                    </Grid>
-
-
-                </Navbar>
-=======
                 <div fixedTop={true} >  
                         <div>
                             <div className="header-body">
@@ -71,21 +32,30 @@ class Header extends React.Component {ł
                                 </div>
                                 <div className="header-input">
                                     <input
-                                        onChange={this.handleChange}
-                                        /*    onChange={this.props.searchByChar(
-                                                this.state.value
-                                            )} */ // Ale to chyba było by zbyt piękne xD bo wali errorem że value jest null na początku
+                                        onChange={this.saveTitleInState}
                                         className="input"
                                         placeholder="Szukaj..."
+                                        onKeyPress={event => {
+                                            if (event.key === 'Enter') {
+                                                this.props.saveTitleToStore(this.state.value)
+                                            }
+                                            if (event.key === 'Enter') {
+                                                this.props.searchMovie(this.state.value)
+                                            }
+                                        }
+                                        }
                                     />
-                                    <Button className="Button"> <FontAwesomeIcon icon={faSearch} /> </Button>
+                                    <Button className="Button"
+                                    onClick={() => {
+                                        this.props.saveTitleToStore(this.state.value)
+                                        this.props.searchMovie(this.state.value)
+                                    }}> <FontAwesomeIcon icon={faSearch} /> </Button>
                                 </div>
                             </div>
                         </div>   
 
                        
                 </div>
->>>>>>> dorota
             </div>
 
         );
