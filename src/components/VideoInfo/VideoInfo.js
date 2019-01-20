4291f21206546e69f7478bc96cd7e6643ba85a66
Radek <laser242424@wp.pl>
2019-01-20 13:40:41 +0100

    small consol log video fix.

--- src/components/VideoInfo/VideoInfo.js
+++ src/components/VideoInfo/VideoInfo.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './VideoInfo.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 class VideInfo extends React.Component {
     constructor(props) {
@@ -21,7 +20,7 @@ class VideInfo extends React.Component {
                 <div className="InfoDisplay">
                     <span className="event" > Wyświetlenia: {this.state.event} </span>
                     <button className="like"  > <FontAwesomeIcon icon={faThumbsUp} /> {this.state.like} </button>
-                    <button className="unLike" > <FontAwesomeIcon size="50px" icon={faThumbsDown} /> {this.state.unlike} </button>
+                    <button className="unLike" > <FontAwesomeIcon icon={faThumbsDown} /> {this.state.unlike} </button>
                 </div>
             </div>
         )
