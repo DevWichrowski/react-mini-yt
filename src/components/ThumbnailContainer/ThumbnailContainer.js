56733982813376da5104545ef95d89357be1087e
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-20 16:01:09 +0100

    Connected ThumbnailContainer to the store.

--- src/components/ThumbnailContainer/ThumbnailContainer.js
+++ src/components/ThumbnailContainer/ThumbnailContainer.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import './ThumbnailContainer.scss';
 import Thumbnail from "../Thumbnail/Thumbnail";
+import connect from "react-redux/es/connect/connect";
 
 class ThumbnailContainer extends Component {
     render() {
-        return (
-            <div className="ThumbnailContainer">
-                <Thumbnail />
-            </div>
-        );
+        const videosArr = this.props.videosData.videos.items;
+        const loading = this.props.videosData.loading;
+
+        if (loading) {
+            return null;
+        }
+
+        if (!loading) {
+            if (videosArr !== undefined) {
+                return (
+                    <div className="ThumbnailContainer">
+                        {videosArr.map((video, index) => {
+                            return (<Thumbnail key={index}/>);
+                        })}
+                    </div>
+                );
+            } else {
+                return null;
+            }
+        }
     }
 }
 
-export default ThumbnailContainer;
+const mapStateToProps = (state) => ({
+    videosData: state.videosData
+});
+
+export default connect(mapStateToProps, null)(ThumbnailContainer);
--- src/components/VideoMain/VideoMain.js
+++ src/components/VideoMain/VideoMain.js
@@ -1,88 +1,87 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './VideoMain.scss';
-import { getVideos } from '../../store/actions/searchVideoAction';
+import {getVideos} from '../../store/actions/searchVideoAction';
 import connect from 'react-redux/es/connect/connect';
 import YouTube from 'react-youtube';
 import VideoInfo from '../VideoInfo/VideoInfo'
 
 class VideoMain extends Component {
-	constructor() {
-		super();
-		this.state = {
-			description: false
-		};
-	}
+    constructor() {
+        super();
+        this.state = {
+            description: false
+        };
+    }
 
-	showDescription = () => {
-		this.setState({ description: !this.state.description });
-	};
+    showDescription = () => {
+        this.setState({description: !this.state.description});
+    };
 
-	render() {
-		const playerSettings = {
-			playerVars: {
-				autoplay: 0
-			}
-		};
+    render() {
+        const playerSettings = {
+            playerVars: {
+                autoplay: 0
+            }
+        };
 
-		const loading = this.props.videosData.loading;
+        const loading = this.props.videosData.loading;
 
-		if (loading) {
-			return (
-				<div className="loading-msg">
-					<h2>Please enter video title</h2>
-				</div>
-			);
-		}
-		if (!loading) {
-			const videosArr = this.props.videosData.videos.items;
-			if (videosArr !== undefined) {
-				return (
-					<div className="VideoMain">
-						{console.log(this.props.videosData.videos.items)}
-						{videosArr.map((item, index) => {
-							if (index === 0) {
-								return (
-									<div key={index}>
-										<YouTube
-											className="main-video"
-											videoId={item.id.videoId}
-											opts={playerSettings}
-										/>
-										<div className="video-details">
-											<h3
-												className="video-title"
-												onClick={() => {
-													this.showDescription();
-												}}
-											>
-												{item.snippet.title}
-											</h3>
-											{this.state.description === true ? <p>{item.snippet.description}</p> : null}
-										</div>
-										<VideoInfo />
-									</div>
-								);
-							}
-						})}
-					</div>
-				);
-			} else {
-				return (
-					<div>
-						<h2>Loading...</h2>
-					</div>
-				);
-			}
-		}
-	}
+        if (loading) {
+            return (
+                <div className="loading-msg">
+                    <h2>Please enter video title</h2>
+                </div>
+            );
+        }
+        if (!loading) {
+            const videosArr = this.props.videosData.videos.items;
+            if (videosArr !== undefined) {
+                return (
+                    <div className="VideoMain">
+                        {videosArr.map((item, index) => {
+                            if (index === 0) {
+                                return (
+                                    <div key={index}>
+                                        <YouTube
+                                            className="main-video"
+                                            videoId={item.id.videoId}
+                                            opts={playerSettings}
+                                        />
+                                        <div className="video-details">
+                                            <h3
+                                                className="video-title"
+                                                onClick={() => {
+                                                    this.showDescription();
+                                                }}
+                                            >
+                                                {item.snippet.title}
+                                            </h3>
+                                            {this.state.description === true ? <p>{item.snippet.description}</p> : null}
+                                        </div>
+                                        <VideoInfo/>
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="loading-msg">
+                        <h2>Loading...</h2>
+                    </div>
+                );
+            }
+        }
+    }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-	getVideos: (payload) => dispatch(getVideos(payload))
+    getVideos: (payload) => dispatch(getVideos(payload))
 });
 
 const mapStateToProps = (state) => ({
-	videosData: state.videosData
+    videosData: state.videosData
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(VideoMain);
