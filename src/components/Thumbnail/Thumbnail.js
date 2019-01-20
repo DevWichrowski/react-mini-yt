4e764ede0b5c27b577fed4c32cab84c283dea951
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-20 14:43:45 +0100

    Add Thumbnail Component

---
+++ src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Thumbnail.scss'
+
+function Thumbnail(props) {
+    return (
+        <div>
+            <h3>Thumb</h3>
+        </div>
+    );
+}
+
+export default Thumbnail;
--- src/components/ThumbnailContainer/ThumbnailContainer.js
+++ src/components/ThumbnailContainer/ThumbnailContainer.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './ThumbnailContainer.scss';
+import Thumbnail from "../Thumbnail/Thumbnail";
 
 class ThumbnailContainer extends Component {
     render() {
         return (
             <div className="ThumbnailContainer">
-                <h2>.</h2>
+                <Thumbnail />
             </div>
         );
     }
