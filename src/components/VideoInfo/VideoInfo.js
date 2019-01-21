19d5813fb4c4ed06b49be09a1029d1fdbc1341f1
Radek <laser242424@wp.pl>
2019-01-21 19:08:50 +0100

    VideoInfo Coponent ready to pick Statistics from store

--- src/components/VideoInfo/VideoInfo.js
+++ src/components/VideoInfo/VideoInfo.js
@@ -2,31 +2,33 @@ import React from 'react'
 import './VideoInfo.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons'
+import connect from 'react-redux/es/connect/connect';
 
 class VideInfo extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            like: 50000,
-            unlike: 1,
-            event: 10000,
-        };
-    }
 
     render() {
+        const videosData = videosData.videosStatistics
         return (
             <div>
                 <div className="InfoDisplay">
-                    <span className="event" > Wyświetlenia: {this.state.event} </span>
-                    <button className="like"  > <FontAwesomeIcon icon={faThumbsUp} /> {this.state.like} </button>
-                    <button className="unLike" > <FontAwesomeIcon icon={faThumbsDown} /> {this.state.unlike} </button>
+                     {videosData.map((item, index) => {
+                      return (
+                    <div key={index}>
+                    <span className="event" > Wyświetlenia: {item.items.statistics.viewCount} </span>
+                    <button className="like"  > <FontAwesomeIcon icon={faThumbsUp} /> {item.items.statistics.likeCount} </button>
+                    <button className="unLike" > <FontAwesomeIcon icon={faThumbsDown} /> {item.items.statistics.dislikeCount} </button>
+                    </div>
+                    )
+                })}
                 </div>
-            </div>
+           </div>
         )
     }
 }
 
+const mapStateToProps = (state) => ({
+    videosData: state.videosData
+});
 
 
-export default VideInfo
+export default connect(mapStateToProps,null) (VideInfo)
