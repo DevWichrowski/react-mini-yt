0c0e2f0a72337275599a8d0713d1cef748753d14
Radek <laser242424@wp.pl>
2019-01-21 20:46:34 +0100

    change component name from VideoInfo to VideoStatistics

--- src/components/VideoMain/VideoMain.js
+++ src/components/VideoMain/VideoMain.js
@@ -3,7 +3,7 @@ import './VideoMain.scss';
 import {getVideos} from '../../store/actions/searchVideoAction';
 import connect from 'react-redux/es/connect/connect';
 import YouTube from 'react-youtube';
-import VideoInfo from '../VideoStatistics/VideoStatistics'
+import VideoStatistics from '../VideoStatistics/VideoStatistics'
 
 class VideoMain extends Component {
     constructor() {
@@ -58,7 +58,7 @@ class VideoMain extends Component {
                                             </h3>
                                             {this.state.description === true ? <p>{item.snippet.description}</p> : null}
                                         </div>
-                                        <VideoInfo/>
+                                        <VideoStatistics/>
                                     </div>
                                 );
                             }
